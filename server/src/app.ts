8f472b413a7d32a65ca91dddecb7736e5e82108b
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-02-25 10:50:54 +0900

    Fix: Server 빌드 오류 해결

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,12 @@ import express, { Request, Response } from "express";
 import path from "path";
 
 const __dirname = path.resolve();
+const FRONTEND_DIR = "/build";
 
 const app = express();
 const port = 5000;
 
-app.use("/", express.static(path.join(__dirname, "/dist")));
+app.use("/", express.static(path.join(__dirname, FRONTEND_DIR)));
 
 app.get("/", (req: Request, res: Response) => {
   res.sendFile("index.html");
