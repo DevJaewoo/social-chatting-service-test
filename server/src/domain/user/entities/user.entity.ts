3fcc8b9aca37a86ffac8c33352c1cf2f795fffed
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-02-25 14:56:42 +0900

    Feat: User Nickname 컬럼 추가

--- server/src/domain/user/entities/user.entity.ts
+++ server/src/domain/user/entities/user.entity.ts
@@ -1,18 +1,23 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, Unique } from "typeorm";
+import { AutoIdEntity } from "../../abstract/auto-id-entity.js";
+import { USER_CONSTANT } from "../../constants.js";
 
-@Entity()
-export class User {
+@Entity({ name: "user" })
+@Unique("unique_name", ["name"])
+@Unique("unique_nickname", ["nickname"])
+export class User extends AutoIdEntity {
+  @Column({
+    type: "varchar",
+    length: USER_CONSTANT.MAX_NAME_LENGTH,
+  })
+  name: string;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column({
+    type: "varchar",
+    length: USER_CONSTANT.MAX_NICKNAME_LENGTH,
+  })
+  nickname: string;
 
-    @Column()
-    firstName: string
-
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
-
+  @Column({ type: "varchar" })
+  password: string;
 }
