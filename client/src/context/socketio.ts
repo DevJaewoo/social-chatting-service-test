d8f22960d894494cfcd6bcd7fd40e65741fb505f
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-02-28 16:48:07 +0900

    Fix: URI 하드코딩으로 인한 배포환경 소켓 연결 문제 해결

--- client/src/context/socketio.ts
+++ client/src/context/socketio.ts
@@ -1,9 +1,9 @@
+/* eslint-disable no-restricted-globals */
 import React from "react";
 import io, { Socket } from "socket.io-client";
 import { ClientEvent, ServerEvent } from "src/socketConstants";
 
-export const socket: Socket<ServerEvent, ClientEvent> = io(
-  "http://localhost:5000"
-);
+const uri = `${location.protocol}//${location.host}`;
+export const socket: Socket<ServerEvent, ClientEvent> = io(uri);
 export const SocketContext =
   React.createContext<Socket<ServerEvent, ClientEvent>>(socket);
